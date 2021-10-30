fb0444744022399480d71d936f0ee65996418eac
JamesWithCode <minseok.jeong.james@gmail.com>
2021-10-30 13:55:18 +0900

    9주차 : 예외처리

---
+++ src/ch9_exceptions/AddContentException.java
@@ -0,0 +1,22 @@
+package ch9_exceptions;
+
+public class AddContentException extends  RuntimeException{
+    private final String code;
+
+    public AddContentException(Throwable cause, String code){
+        super(cause);
+        this.code=code;
+    }
+    public String getCode(){
+        return code;
+    }
+    private void addContent(){
+        try{
+            //컨텐츠를 추가하는 로직
+        }catch(SomeException 1 || SomeException2 e){
+            throw new AddContentException(e,e.getCode());
+        }catch(SomeException 3){
+            throw new AddContentException(e,"SE3");
+        }
+    }
+}
