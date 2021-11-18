284e886021d03c43605856704ec355e9ff445f9a
JamesWithCode <minseok.jeong.james@gmail.com>
2021-11-18 21:24:14 +0900

    11주차 enum - 1

---
+++ src/ch11_enum/EnumDemo1.java
@@ -0,0 +1,27 @@
+package ch11_enum;
+
+public class EnumDemo1 {
+    enum Season{SPRING,SUMMER,FALL,WINTER}
+
+    public static void main(String[] args) {
+        printSeason(Season.SUMMER);
+    }
+    public static void printSeason(Season season){
+        switch(season){
+            case SPRING:
+                System.out.println("봄입니다.");
+                break;
+            case SUMMER:
+                System.out.println("여름입니다.");
+                break;
+            case FALL:
+                System.out.println("가을입니다.");
+                break;
+            case WINTER:
+                System.out.println("겨울입니다.");
+                break;
+            default:
+                throw new IllegalArgumentException("잘못된 계절입니다.");
+        }
+    }
+}
---
+++ src/ch11_enum/EnumDemo4.java
@@ -0,0 +1,8 @@
+package ch11_enum;
+
+public class EnumDemo4 {
+    public static void main(String[] args) {
+        Season.SUMMER.rangeTemperature(6);
+        Season.WINTER.rangeTemperature(10);
+    }
+}
