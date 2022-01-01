467473c606a5f49a92a17f75b9e9dfa55d299276
JamesWithCode <minseok.jeong.james@gmail.com>
2022-01-01 18:40:15 +0900

    환경설정
    UTF-8 한글설정
    컨텍스트 패스 설정

--- java-reflection-practice/out/artifacts/java_reflection_practice_war_exploded/index.jsp
+++ java-reflection-practice/out/artifacts/java_reflection_practice_war_exploded/index.jsp
@@ -12,6 +12,5 @@
   </head>
   <body>
   <h1>인덱스 페이지입니다.</h1>
-  $END$
   </body>
 </html>
---
+++ java-reflection-practice/src/com/cos/reflect/filter/Dispatcher.java
@@ -0,0 +1,18 @@
+package com.cos.reflect.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Dispatcher implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request =(HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+
+        System.out.println("컨텍스트 패스 : "+ request.getContextPath()); //프로젝스 시작 주소
+        System.out.println("식별자 주소 : " + request.getRequestURI()); //전체 주소
+        System.out.println("전체 주소 : "+request.getRequestURL());
+    }
+}
