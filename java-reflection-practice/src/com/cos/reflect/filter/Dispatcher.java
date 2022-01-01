db3f48c01736231228f601d0e747f84daa30493f
JamesWithCode <minseok.jeong.james@gmail.com>
2022-01-01 21:20:49 +0900

    어노테이션 만들기 완료

--- java-reflection-practice/src/com/cos/reflect/filter/Dispatcher.java
+++ java-reflection-practice/src/com/cos/reflect/filter/Dispatcher.java
@@ -14,5 +14,9 @@ public class Dispatcher implements Filter {
         System.out.println("컨텍스트 패스 : "+ request.getContextPath()); //프로젝스 시작 주소
         System.out.println("식별자 주소 : " + request.getRequestURI()); //전체 주소
         System.out.println("전체 주소 : "+request.getRequestURL());
+
+        String endPoint = request.getRequestURI().replaceAll(request.getContextPath(),"");
+        System.out.println("엔드 포인트 : "+endPoint);
+
     }
 }
