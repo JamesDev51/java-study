335469dc94a64ccdd8ab65125b4b1bbd5617345f
JamesWithCode <minseok.jeong.james@gmail.com>
2023-03-11 13:23:34 +0900

    220311 effective-java Item 4 인스턴스화를 막으려거든 private 생성자를 사용하라

--- effective-java/src/main/java/me/whiteship/chapter01/item04/DefaultUtilityClass.java
+++ effective-java/src/main/java/me/whiteship/chapter01/item04/DefaultUtilityClass.java
@@ -3,6 +3,7 @@ package me.whiteship.chapter01.item04;
 public class DefaultUtilityClass extends UtilityClass {
 
     public static void main(String[] args) {
+        //서브클래스를 만들면 인스턴스를 만들 수 있게 됨
         DefaultUtilityClass utilityClass = new DefaultUtilityClass();
         utilityClass.hello();
     }
